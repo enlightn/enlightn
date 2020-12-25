b6c9bc97e4dcbd4b977b6217045ab98803964bf8
Paras Malhotra <paras@insenseanalytics.com>
2020-12-25 18:44:37 +0530

    wip

--- src/Analyzers/Security/FilePermissionsAnalyzer.php
+++ src/Analyzers/Security/FilePermissionsAnalyzer.php
@@ -42,8 +42,8 @@ class FilePermissionsAnalyzer extends SecurityAnalyzer
         return "Your application's project directory permissions are not setup in a secure manner. This may "
             ."expose your application to be compromised if another account on the same server is vulnerable. "
             ."This can be even more dangerous if you used shared hosting. All project directories in Laravel "
-            ."should be setup with a max of 755 permissions and most app files should be provided 644 (except "
-            ."executables such as Artisan or your deployment scripts which should be provided 755 permissions). "
+            ."should be setup with a max of 775 permissions and most app files should be provided 664 (except "
+            ."executables such as Artisan or your deployment scripts which should be provided 775 permissions). "
             ."These are the max level of permissions in order to be secure. Your unsafe files or directories "
             ."include: {$this->unsafeFilesOrDirs}.";
     }
@@ -56,20 +56,20 @@ class FilePermissionsAnalyzer extends SecurityAnalyzer
     public function handle()
     {
         $filesOrDirectoriesToCheck = config('enlightn.allowed_permissions', [
-            base_path() => '755',
-            app_path() => '755',
-            resource_path() => '755',
-            storage_path() => '755',
-            public_path() => '755',
-            config_path() => '755',
-            database_path() => '755',
-            base_path('routes') => '755',
-            app()->bootstrapPath() => '755',
-            app()->bootstrapPath('cache') => '755',
-            app()->bootstrapPath('app.php') => '644',
-            base_path('artisan') => '755',
-            public_path('index.php') => '644',
-            public_path('server.php') => '644',
+            base_path() => '775',
+            app_path() => '775',
+            resource_path() => '775',
+            storage_path() => '775',
+            public_path() => '775',
+            config_path() => '775',
+            database_path() => '775',
+            base_path('routes') => '775',
+            app()->bootstrapPath() => '775',
+            app()->bootstrapPath('cache') => '775',
+            app()->bootstrapPath('app.php') => '664',
+            base_path('artisan') => '775',
+            public_path('index.php') => '664',
+            public_path('server.php') => '664',
         ]);
 
         $this->unsafeFilesOrDirs = collect($filesOrDirectoriesToCheck)->filter(function ($allowedPermission, $path) {
