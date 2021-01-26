4cc29f3d87cf1c96f6b4f8ac72406b6e8a49a65e
Paras Malhotra <paras@insenseanalytics.com>
2021-01-26 11:38:52 +0530

    Add CC0 and Unlicense to safe license list

--- config/enlightn.php
+++ config/enlightn.php
@@ -100,6 +100,7 @@ return [
     'license_whitelist' => [
         'Apache-2.0', 'Apache2', 'BSD-2-Clause', 'BSD-3-Clause', 'LGPL-2.1-only', 'LGPL-2.1',
         'LGPL-2.1-or-later', 'LGPL-3.0', 'LGPL-3.0-only', 'LGPL-3.0-or-later', 'MIT', 'ISC',
+        'CC0-1.0', 'Unlicense',
     ],
 
     // List your commercial packages (licensed by you) below, so that they are not
