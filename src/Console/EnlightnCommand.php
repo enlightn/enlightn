c29c761f86f09d96fa96c69f00a210ab05a43266
Paras Malhotra <paras@insenseanalytics.com>
2021-01-29 13:29:45 +0530

    Fix percentage calculations

--- src/Console/EnlightnCommand.php
+++ src/Console/EnlightnCommand.php
@@ -206,7 +206,9 @@ class EnlightnCommand extends Command
      * @return string
      */
     protected function formatResult($status, $category) {
-        $totalAnalyzersInCategory = (float) collect($this->result[$category])->sum(function ($count) {
+        $totalAnalyzersInCategory = (float) collect($this->result[$category])->filter(function($result, $status) {
+            return in_array($status, ['passed', 'failed', 'skipped', 'error']);
+        })->sum(function ($count) {
             return $count;
         });
 
