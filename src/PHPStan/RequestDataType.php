e8e8c993c9ce0482237ad48e40a327ad359c1a5c
Paras Malhotra <paras@insenseanalytics.com>
2021-01-31 15:49:58 +0530

    Fix request data type detection

--- src/PHPStan/RequestDataType.php
+++ src/PHPStan/RequestDataType.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Enlightn\Enlightn\PHPStan;
 
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\CompoundType;
 use PHPStan\Type\StringType;
 use PHPStan\Type\Type;
 
@@ -16,5 +18,16 @@ class RequestDataType extends StringType
     public static function __set_state(array $properties): Type
     {
         return new self();
+    }
+
+    public function isSuperTypeOf(Type $type) : TrinaryLogic
+    {
+        if ($type instanceof self) {
+            return TrinaryLogic::createYes();
+        }
+        if ($type instanceof CompoundType) {
+            return $type->isSubTypeOf($this);
+        }
+        return TrinaryLogic::createNo();
     }
 }
